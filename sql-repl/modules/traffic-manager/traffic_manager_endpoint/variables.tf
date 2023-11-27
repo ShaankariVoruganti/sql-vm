8c5462c1a68dbad8e4765ff4c6c1e6aeaa7207e8
ShaankariVoruganti <67142384+ShaankariVoruganti@users.noreply.github.com>
2023-11-27 10:16:19 +0530

    Update variables.tf

--- sql-repl/modules/traffic-manager/traffic_manager_endpoint/variables.tf
+++ sql-repl/modules/traffic-manager/traffic_manager_endpoint/variables.tf
@@ -2,7 +2,7 @@ variable endpoint1_name{}
 variable weight1{}
 variable endpoint2_name{}
 variable weight2{}
-variable vm1_pip{}
-variable vm2_pip{}
+variable pip1_id{}
+variable pip2_id{}
 variable traffic_manager_profile_id{}
 
